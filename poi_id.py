77466fd2ad12419f0da6d478d817f4c5ff40a9aa
wujiyan <fretwan@163.com>
2016-06-24 17:10:00 -0500

    Add files via upload

--- poi_id.py
+++ poi_id.py
@@ -5,7 +5,6 @@ import numpy as np
 from sklearn.feature_selection import SelectKBest
 from sklearn.decomposition import PCA
 from sklearn.feature_selection import chi2
-from sklearn.preprocessing import MinMaxScaler
 from sklearn import cross_validation
 from sklearn import tree
 from sklearn.metrics import accuracy_score
@@ -15,6 +14,7 @@ from sklearn.svm import SVC
 from sklearn.metrics import precision_score
 from sklearn.metrics import recall_score
 import sklearn.pipeline
+from sklearn.preprocessing import Imputer
 
 #define a function to make scatter plot
 def scatter_plot(data, xname, yname, title, color = "blue"):
@@ -33,44 +33,7 @@ def scatter_plot(data, xname, yname, title, color = "blue"):
     plt.title(title.split(".")[0])
     plt.savefig(title, bbox_inches='tight')
 
-#read dataset from local
-with open("final_project_dataset.pkl", "r") as data_file:
-    rawdata = pickle.load(data_file)
-
-mydata = rawdata
-#transfer data from dictionary to data frame. refer 1
-mydata_df = pd.DataFrame.from_dict(data = mydata, orient = 'index')
-
-print("show the list of column names:")
-print(list(mydata_df.columns.values))
-print("total number of data points:")
-print(len(mydata))
-#exploration
-
-#plot relationship between total stock value and total payments
-scatter_plot(mydata_df, "salary",
-             "bonus","salary_vs_bonus.png")
-
-#drop TOTAL, the outlier, and scatter again
-#mydata.pop('TOTAL')
-#mydata_df1 = pd.DataFrame.from_dict(data = mydata, orient = 'index')
-mydata_df1 = mydata_df.drop("TOTAL")
-scatter_plot(mydata_df1, "salary",
-             "bonus","salary_vs_bonus2.png", color = mydata_df1['poi'])
-print("scatter plot: salary vs bonus, has been done")
-
-#plot scatter plot: total payments vs expenses
-mydata_df_new = mydata_df1.drop("LAY KENNETH L")
-scatter_plot(mydata_df_new, "total_payments",
-             "expenses","total_payments_vs_expenses.png", color = mydata_df_new['poi'])
-print("scatter plot: total_payments_vs_expenses, has been done")
-
-#plot scatter plot: to_messages vs from_messages
-scatter_plot(mydata_df1, "to_messages",
-             "from_messages","to_messages_vs_from_messages.png", color = mydata_df1['poi'])
-print("scatter plot: to_messages_vs_from_messages, has been done")
-
-#add new features, fraction of sending to poi and fraction of receiving from poi
+#define a function to create new features
 def create_feature(data, lab1, lab2, new_lab):
     ## input:
     # data: original dataset
@@ -85,45 +48,7 @@ def create_feature(data, lab1, lab2, new_lab):
             data[names][new_lab] = float(data[names][lab1])/float(data[names][lab2])
     return data
 
-mydata = create_feature(mydata, "from_this_person_to_poi",\
-                        "from_messages","fraction_to_poi")
-mydata = create_feature(mydata, "from_poi_to_this_person",\
-                        "to_messages", "fraction_from_poi")
-print("features have been added")
-
-#plot scatter plot: fraction to poi vs fraction from poi
-mydata_df2 = pd.DataFrame.from_dict(data = mydata, orient = 'index')
-scatter_plot(mydata_df2, "fraction_to_poi",
-             "fraction_from_poi","fraction_to_poi_vs_fraction_from_poi.png", color = mydata_df2['poi'])
-print("scatter plot: fraction_to_poi_vs_fraction_from_poi, has been done")
-
-#count NaN number
-mydata_df3 = mydata_df2.drop(["email_address",'poi','fraction_from_poi','fraction_to_poi'],1)
-missing_number = {}
-for column in mydata_df3.columns.values:
-    v = len(mydata_df3[mydata_df3[column] == 'NaN'])
-    missing_number[column] = v
-missing_number_df = pd.DataFrame.from_dict(data = missing_number, orient = 'index')
-print("show the number of missing values:")
-print(missing_number_df)
-
-#set up feature list. include all first
-#this is the original features
-features_list = ['salary','deferral_payments','deferred_income','director_fees',
-                 'exercised_stock_options','expenses',
-                 'fraction_from_poi','fraction_to_poi','from_messages',
-                 'from_poi_to_this_person','from_this_person_to_poi',
-                 'loan_advances','long_term_incentive','other','poi',
-                 'restricted_stock','restricted_stock_deferred','salary',
-                 'shared_receipt_with_poi','to_messages','total_payments',
-                 'total_stock_value']
-#this is the simplified features. How they were selected are explained in pdf report.
-features_list_sim = ['salary', 'exercised_stock_options','expenses',
-                 'fraction_from_poi','fraction_to_poi',
-                 'long_term_incentive','other','poi','restricted_stock',
-                 'salary','shared_receipt_with_poi',
-                 'total_payments','total_stock_value']
-#change data format, shifting NaN into 0. remove keys without valid values
+#change data format, shifting NaN into 0.
 #extract poi labels and features
 def change_format(data, features):
     ##input: 
@@ -147,21 +72,39 @@ def change_format(data, features):
             if feature == "poi":      # seperate poi label 
                 label_list.append(float(value))
                 continue
-            elif value == "NaN":     #change NaN value into 0
-                value = 0
             elif value < 0:
                 value = -value     #change negative values into positive ones.
             temp_list.append(float(value))
         final_list.append(np.array(temp_list))
     return np.array(final_list), np.array(label_list)
-
-features_data, labels_data= change_format(mydata, features_list)
-features_data_sim, labels_data_sim = change_format(mydata, features_list_sim)
-
-#scale data
-scaler = MinMaxScaler()
-mydata_scl = scaler.fit_transform(features_data)
-mydata_scl_sim = scaler.fit_transform(features_data_sim)
+#define a function to scale value. In this function, NaN will remained NaN
+#other numeric value will distributed between 0 and 1 according to minmaxscaler. 
+def scaler(data, feature_num):
+    #input: original data(list)
+    #output: new data(same format)
+    f = pd.DataFrame(features_data)
+    i = 0
+    j = 0
+    list2 = []
+    while j < len(features_data):
+        h_1 = f.iloc[j]
+        list1 = []
+        i = 0
+        while i <= feature_num-1:
+            f_1 = f[i]
+            j_1 = h_1[i]
+            min_value = min(f_1[f_1.isnull() == False])
+            max_value = max(f_1[f_1.isnull() == False])
+            if j_1 == "NaN":
+                list1.append("dd")
+            else:
+                new_val = (j_1-min_value)/(max_value-min_value)
+                list1.append(new_val)
+            i = i+1
+        list2.append(list1)
+        j = j+1
+    
+    return list2
 
 #select top 10 features having the closest relationship with poi label
 def select_best_features(features_data, labels, features_list, number = 10):
@@ -173,27 +116,7 @@ def select_best_features(features_data, labels, features_list, number = 10):
     for feature, score in zip(features_list, scores):
         dic[feature] = score
     new_dic = sorted(dic.iteritems(), key=lambda d:d[1], reverse = True)
-    return f2, new_dic[0:12]
-
-best_features, best_data= select_best_features(mydata_scl, labels_data, features_list)
-best_features_sim, best_data_sim = select_best_features(mydata_scl_sim, labels_data_sim, features_list_sim)
-print("show features by score")
-print(list(best_data_sim))
-
-#features I selected
-features = ["poi","fraction_to_poi","fraction_from_poi","shared_receipt_with_poi"]
-
-clf1 = RandomForestClassifier(random_state = 50,min_samples_split=5)
-clf2 = GaussianNB()
-clf3 = tree.DecisionTreeClassifier(random_state = 6,min_samples_split = 6)
-#get pure data and poi labels
-feature, labels = change_format(mydata, features)
-#scale the data
-scaler = MinMaxScaler()
-feature_scl = scaler.fit_transform(feature)
-#split data into train and test set.
-features_train, features_test, labels_train, labels_test = \
-cross_validation.train_test_split(feature_scl, labels, test_size=0.3, random_state = 134)
+    return new_dic[0:12]
 
 #define a function to train and test, returning performance report
 def report(clf, features_train, features_test, labels_train, labels_test):
@@ -211,6 +134,134 @@ def report(clf, features_train, features_test, labels_train, labels_test):
     report = sklearn.metrics.classification_report( labels_test, y_prediction )
 
     return report
+
+#read dataset from local
+with open("final_project_dataset.pkl", "r") as data_file:
+    rawdata = pickle.load(data_file)
+mydata = rawdata
+
+#transfer data from dictionary to data frame.
+mydata_df = pd.DataFrame.from_dict(data = mydata, orient = 'index')
+#exploration
+print("Show the list of column names:")
+print(list(mydata_df.columns.values))
+print("Total number of data points:")
+print(len(mydata))
+print("Number of POIs:")
+print(len(mydata_df[mydata_df.poi == True]))
+print("Number of non-POIs:")
+print(len(mydata_df[mydata_df.poi == False]))
+#plot relationship between total stock value and total payments
+scatter_plot(mydata_df, "salary",
+             "bonus","salary_vs_bonus.png")
+
+#drop TOTAL, the outlier, and scatter again
+mydata.pop('TOTAL')
+mydata_df1 = pd.DataFrame.from_dict(data = mydata, orient = 'index')
+#mydata_df1 = mydata_df.drop("TOTAL")
+scatter_plot(mydata_df1, "salary",
+             "bonus","salary_vs_bonus2.png", color = mydata_df1['poi'])
+print("scatter plot: salary vs bonus, has been done")
+
+#plot scatter plot: total payments vs expenses
+mydata_df_new = mydata_df1.drop("LAY KENNETH L")
+scatter_plot(mydata_df_new, "total_payments",
+             "expenses","total_payments_vs_expenses.png", color = mydata_df_new['poi'])
+print("scatter plot: total_payments_vs_expenses, has been done")
+
+#plot scatter plot: to_messages vs from_messages
+scatter_plot(mydata_df1, "to_messages",
+             "from_messages","to_messages_vs_from_messages.png", color = mydata_df1['poi'])
+print("scatter plot: to_messages_vs_from_messages, has been done")
+
+#add new features, fraction of sending to poi and fraction of receiving from poi
+mydata = create_feature(mydata, "from_this_person_to_poi",\
+                        "from_messages","fraction_to_poi")
+mydata = create_feature(mydata, "from_poi_to_this_person",\
+                        "to_messages", "fraction_from_poi")
+print("features have been added")
+
+#plot scatter plot: fraction to poi vs fraction from poi
+mydata_df2 = pd.DataFrame.from_dict(data = mydata, orient = 'index')
+scatter_plot(mydata_df2, "fraction_to_poi",
+             "fraction_from_poi","fraction_to_poi_vs_fraction_from_poi.png", color = mydata_df2['poi'])
+print("scatter plot: fraction_to_poi_vs_fraction_from_poi, has been done")
+
+#count NaN number
+mydata_df3 = mydata_df2.drop(["email_address",'poi','fraction_from_poi','fraction_to_poi'],axis = 1)
+missing_number = {}
+for column in mydata_df3.columns.values:
+    v = len(mydata_df3[mydata_df3[column] == 'NaN'])
+    missing_number[column] = v
+missing_number_df = pd.DataFrame.from_dict(data = missing_number, orient = 'index')
+print("show the number of missing values:")
+print(missing_number_df)
+
+#set up feature list. include all first
+#this is the original features
+features_list = ["poi", 'salary','deferral_payments','deferred_income','director_fees',
+                 'exercised_stock_options','expenses',
+                 'fraction_from_poi','fraction_to_poi','from_messages',
+                 'from_poi_to_this_person','from_this_person_to_poi',
+                 'loan_advances','long_term_incentive','other',
+                 'restricted_stock','restricted_stock_deferred',
+                 'shared_receipt_with_poi','to_messages','total_payments',
+                 'total_stock_value']
+#this is the simplified features. How they were selected are explained in pdf report.
+features_list_sim2 = ["poi", 'salary','deferral_payments','deferred_income','director_fees',
+                 'exercised_stock_options','expenses',
+                 'fraction_from_poi','fraction_to_poi',
+                 'loan_advances','long_term_incentive','other',
+                 'restricted_stock','restricted_stock_deferred',
+                 'shared_receipt_with_poi','total_payments',
+                 'total_stock_value']
+#get pure data and poi labels
+features_data_sim, labels_data_sim = change_format(mydata, features_list_sim2)
+
+#scale data for selecting features
+features_data_scl = scaler(features_data_sim, len(features_list_sim2)-1)
+
+#replacing NaN with median
+imp = Imputer(missing_values='NaN', strategy='median', axis=0)
+features_data_scl1 = imp.fit_transform(features_data_sim)
+features_data_scl2 = imp.fit_transform(features_data_scl)
+
+#split data into train and test set for non-scaled data.
+features_train, features_test, labels_train, labels_test = \
+cross_validation.train_test_split(features_data_scl1, labels_data_sim, test_size=0.3, random_state = 170,stratify = labels_data_sim )
+
+#select best features: decisiontree
+clf = tree.DecisionTreeClassifier(min_samples_split=5)
+clf = clf.fit(features_train, labels_train)
+print("show feature scores by decision tree")
+for i in range(len(clf.feature_importances_)):
+    if clf.feature_importances_[i] > .00005:
+        print "{}:{}".format(features_list_sim2[i+1],clf.feature_importances_[i] )
+        
+#split data into train and test set for scaled data.
+features_train, features_test, labels_train, labels_test = \
+cross_validation.train_test_split(features_data_scl2, labels_data_sim, test_size=0.3, random_state = 170,stratify = labels_data_sim )
+
+#select best features: selectkbest 
+best_data= select_best_features(features_data_scl2, labels_data_sim, features_list_sim2)
+print("show features by selectkbest score:")
+print(list(best_data))
+
+#replacing NaN with median in non-scaled data
+imp = Imputer(missing_values='NaN', strategy='median', axis=0)
+features_data_scl2 = imp.fit_transform(features_data_sim)
+
+#features I selected
+features = ['poi','fraction_to_poi','exercised_stock_options','deferred_income','shared_receipt_with_poi']
+
+clf1 = RandomForestClassifier(random_state = 50,min_samples_split=8)
+clf2 = GaussianNB()
+clf3 = tree.DecisionTreeClassifier(random_state = 6,min_samples_split = 12)
+
+#get pure data and poi labels
+#feature, labels = change_format(mydata, features)
+#scale the data
+#features_scl = scaler(features, len(features)-1)
 
 #print out report
 print("Random Forest Report")
@@ -321,11 +372,13 @@ print(report(clf3,features_train, features_test, labels_train, labels_test))
 #cross_validation.train_test_split(feature_scl, labels, test_size=0.3, random_state = 1)
 #ml_result = ml_deploy(features_train, features_test, labels_train, labels_test)
 
+
 ### dump your classifier, dataset and features_list so
 ### anyone can run/check your results
-clf = clf3.fit(features_train, labels_train)
+clf = clf2.fit(features_train, labels_train)
 data_dict = mydata
 pickle.dump(clf, open("my_classifier.pkl", "w") )
 pickle.dump(data_dict, open("my_dataset.pkl", "w") )
 pickle.dump(features, open("my_feature_list.pkl", "w") )
+
 
